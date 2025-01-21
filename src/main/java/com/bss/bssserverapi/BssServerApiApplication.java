820c0f46ab330b204d8c5fd8d88a2e588d423163
whitej00 <wlsdka12@gmail.com>
2025-01-21 14:54:02 +0900

    init

---
+++ src/main/java/com/bss/bssserverapi/BssServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.bss.bssserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BssServerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BssServerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bss/bssserverapi/BssServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bss.bssserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BssServerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
