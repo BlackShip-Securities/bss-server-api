6183e0bb4773193a77cb0e744238a189b76c3d90
whitej00 <wlsdka12@gmail.com>
2025-03-20 21:29:00 +0900

    [feat-#62] 리서치 단일 상세 조회 구현 완료

--- src/main/java/com/bss/bssserverapi/domain/research/dto/GetResearchPreviewResDto.java
+++ src/main/java/com/bss/bssserverapi/domain/research/dto/GetResearchPreviewResDto.java
@@ -2,14 +2,17 @@ package com.bss.bssserverapi.domain.research.dto;
 
 import com.bss.bssserverapi.domain.research.Research;
 import com.bss.bssserverapi.domain.tag.Tag;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class GetResearchPreviewResDto {
 
+    private final Long id;
     private final String title;
     private final Long targetPrice;
     private final LocalDate dateStart;
@@ -17,19 +20,10 @@ public class GetResearchPreviewResDto {
     private final String userName;
     private final List<String> tagNameList;
 
-    public GetResearchPreviewResDto(final String title, final Long targetPrice, final LocalDate dateStart, final LocalDate dateEnd, final String userName, final List<String> tagNameList) {
-
-        this.title = title;
-        this.targetPrice = targetPrice;
-        this.dateStart = dateStart;
-        this.dateEnd = dateEnd;
-        this.userName = userName;
-        this.tagNameList = tagNameList;
-    }
-
     public static GetResearchPreviewResDto toDto(final Research research, final List<Tag> tagList) {
 
         return new GetResearchPreviewResDto(
+                research.getId(),
                 research.getTitle(),
                 research.getTargetPrice(),
                 research.getDateStart(),
