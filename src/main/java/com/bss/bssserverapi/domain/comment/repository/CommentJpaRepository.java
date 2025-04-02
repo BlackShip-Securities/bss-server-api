73d4bee82863f30d43112a62f17800fd6bd5f166
whitej00 <wlsdka12@gmail.com>
2025-04-02 16:26:35 +0900

    [feat-#84] 10개씩 페이징 조회로 변경

--- src/main/java/com/bss/bssserverapi/domain/comment/repository/CommentJpaRepository.java
+++ src/main/java/com/bss/bssserverapi/domain/comment/repository/CommentJpaRepository.java
@@ -1,11 +1,11 @@
 package com.bss.bssserverapi.domain.comment.repository;
 
 import com.bss.bssserverapi.domain.comment.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findCommentsByResearchIdAndParentCommentIsNull(final Long researchId);
+    Page<Comment> findCommentsByResearchIdAndParentCommentIsNull(final Long researchId, final Pageable pageable);
 }
